dfaee19a2ef746ba91953185cef3d10f0c3d936e
coderboy-raiyan <tajkierhaque@gmail.com>
2025-10-18 15:59:03 +0600

    hero section UI done

--- src/pages/home/hero.jsx
+++ src/pages/home/hero.jsx
@@ -9,12 +9,12 @@ function Hero() {
          border-[3px] rounded-2xl border-[#fff] p-4 my-4"
       >
         {/* hero title */}
-        <h1 className="w-full text-4xl font-extrabold text-center flex-1">
+        <h1 className="w-full text-xl md:text-4xl font-extrabold text-center flex-1">
           Dependable Care, Backed by Trusted <br /> Professionals.
         </h1>
 
         {/* hero des */}
-        <p className="w-full text-center text-[#0F0F0F] font-[500] text-sm flex-1">
+        <p className="md:w-[80%] w-full text-center text-[#0F0F0F] font-[500] text-sm flex-1">
           Our platform connects you with verified, experienced doctors across
           various specialties — all at your convenience. Whether it's a routine
           checkup or urgent consultation, book appointments in minutes and
@@ -22,15 +22,21 @@ function Hero() {
         </p>
 
         {/* hero search */}
-        <div className="w-full flex items-center">
-          <input className="input" type="text" />
-          <button className="btn-primary">Search Now</button>
+        <div className="w-full flex items-center justify-center sm:space-y-0 space-y-4 sm:space-x-4 flex-col sm:flex-row">
+          <input
+            className="input rounded-full py-4 px-4 placeholder:text-xs md:w-2/4 w-2/3"
+            placeholder="Search any doctor..."
+            type="text"
+          />
+          <button className="btn-primary py-4 md:w-[15%] w-2/3">
+            Search Now
+          </button>
         </div>
 
         {/* hero gallery */}
         <div className="w-full flex items-center justify-center space-x-4">
           <img className="w-[40%]" src={images.bannerImg} alt="" />
-          <img className="w-[40%]" src={images.bannerImg} alt="" />s
+          <img className="w-[40%]" src={images.bannerImg} alt="" />
         </div>
       </div>
     </section>
